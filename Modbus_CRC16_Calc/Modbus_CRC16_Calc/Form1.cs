96f5439088c102e3338dcf7ec1d7df4a27ddc538
sferryUL <40303819+sferryUL@users.noreply.github.com>
2018-06-22 15:17:22 -0500

    Initial Modbus class creation

--- Modbus_CRC16_Calc/Modbus_CRC16_Calc/Form1.cs
+++ Modbus_CRC16_Calc/Modbus_CRC16_Calc/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Modbus;
 
 namespace Modbus_CRC16_Calc
 {
---
+++ Modbus_CRC16_Calc/Modbus_CRC16_Calc/Modbus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modbus
+{
+    class ModbusCRC16
+    {
+        struct Message
+        {
+            byte ToAddr;
+            byte FuncCode;
+            List<byte> Payload;
+            ushort CRC16;
+        }
+
+        Message OutMessage, InMessage;
+
+        public void ConstructMessage(byte p_To, byte p_Code, List<byte> p_Payload, uint CRC16)
+        {
+            CalcCRC16(p_Payload);
+        }
+
+        private ushort CalcCRC16(List<byte> p_DataBuffer)
+        {
+            ushort CRCResult = 0xFFFF, XORVal = 0xA001, XOR = 0x0000;
+
+            for (int i = 0; i < p_DataBuffer.Count; i++)
+            {
+                CRCResult ^= p_DataBuffer[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    XOR = (ushort)(CRCResult & 0x0001);
+                    CRCResult >>= 1;
+
+                    if (XOR > 0)
+                        CRCResult ^= XORVal;
+                }
+            }
+
+            return CRCResult;
+        }
+    }
+}
